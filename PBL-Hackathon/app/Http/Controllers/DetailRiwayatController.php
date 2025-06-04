8c8cc6cfe9901dd10a48fe83ba394526364c83a6
Zaini Ridha <zainiridha8@Gmail.com>
2025-06-04 21:12:56 +0700

    update riwayat dan detail

--- PBL-Hackathon/app/Http/Controllers/DetailRiwayatController.php
+++ PBL-Hackathon/app/Http/Controllers/DetailRiwayatController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Pendaftaran;
 
 class DetailRiwayatController extends Controller
 {
-     public function detailRiwayat()
+    public function detailRiwayat($id)
     {
-        // Ganti 'beranda.trainee' dengan nama file view kamu
-        return view('Traineev2.DetailRiwayat');
+        $pendaftaran = Pendaftaran::with('kursus.ratingKursus')->findOrFail($id);
+
+        // Hitung rata-rata rating dan jumlah ulasan
+        $averageRating = $pendaftaran->kursus->ratingKursus->avg('rating');
+        $totalReview = $pendaftaran->kursus->ratingKursus->count();
+
+        return view('Traineev2.DetailRiwayat', compact('pendaftaran', 'averageRating', 'totalReview'));
     }
 }
--- PBL-Hackathon/app/Http/Controllers/RiwayatController.php
+++ PBL-Hackathon/app/Http/Controllers/RiwayatController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Pendaftaran;
+
 
 class RiwayatController extends Controller
 {
-     public function riwayat()
+    public function riwayat()
     {
-        // Ganti 'beranda.trainee' dengan nama file view kamu
-        return view('Traineev2.Riwayat');
+        $user = Auth::user();
+
+        // Ambil semua pendaftaran yang terkait dengan user
+        $riwayat = Pendaftaran::with('kursus')
+            ->where('pengguna_id', $user->pengguna_id)
+            ->get();
+
+        return view('Traineev2.Riwayat', compact('riwayat', 'user'));
     }
 }
