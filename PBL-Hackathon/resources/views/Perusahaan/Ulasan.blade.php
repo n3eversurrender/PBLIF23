a8cbc912205a628615e161dcc0727e00df03b268
Zaini Ridha <zainiridha8@Gmail.com>
2025-06-10 22:28:25 +0700

    update ulasan

--- PBL-Hackathon/resources/views/Perusahaan/Ulasan.blade.php
+++ PBL-Hackathon/resources/views/Perusahaan/Ulasan.blade.php
@@ -14,22 +14,28 @@
             </tr>
         </thead>
         <tbody class="divide-y divide-gray-100">
-            <!-- Baris Kursus 1 -->
+            @forelse ($dataKursusUlasan as $index => $kursus)
             <tr class="hover:bg-gray-50">
-                <td class="px-4 py-3">1</td>
-                <td class="px-4 py-3 font-medium text-gray-800">Welding Pro</td>
-                <td class="px-4 py-3 text-gray-600">20</td>
-                <td class="px-4 py-3 text-gray-600"> <i class="fas fa-star text-yellow-400 text-sm me-2"></i>7.5</td>
+                <td class="px-4 py-3">{{ $index + 1 }}</td>
+                <td class="px-4 py-3 font-medium text-gray-800">{{ $kursus['nama_kursus'] }}</td>
+                <td class="px-4 py-3 text-gray-600">{{ $kursus['jumlah_ulasan'] }}</td>
+                <td class="px-4 py-3 text-gray-600">
+                    <i class="fas fa-star text-yellow-400 text-sm me-2"></i>
+                    {{ $kursus['rata_rata_rating'] }}
+                </td>
                 <td class="px-4 py-3 space-x-2">
                     <div class="flex gap-4 h-full">
-                        <a href="/detailulasan" class="edit-button font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                        <a href="/detailulasan/{{ $kursus['kursus_id'] }}" class="edit-button font-medium text-blue-600 dark:text-blue-500 hover:underline">
                             <i class="fas fa-edit"></i> Detail
                         </a>
                     </div>
                 </td>
             </tr>
-
-            <!-- Tambahkan baris kursus lainnya di sini -->
+            @empty
+            <tr>
+                <td colspan="5" class="px-4 py-3 text-center text-gray-600">Tidak ada data kursus yang tersedia.</td>
+            </tr>
+            @endforelse
         </tbody>
     </table>
 </main>
