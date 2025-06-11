c8ec25db089114d3076ad07df6fb22f800baa0b6
Zaini Ridha <zainiridha8@Gmail.com>
2025-06-11 22:46:27 +0700

    update tambah kursus

--- PBL-Hackathon/resources/views/Perusahaan/TambahKursus.blade.php
+++ PBL-Hackathon/resources/views/Perusahaan/TambahKursus.blade.php
@@ -16,14 +16,16 @@
     </div>
 
     <h2 class="text-2xl font-bold text-center mb-5">Tambah Kursus</h2>
-    <form action="">
+    <form action="{{ route('SimpanKursus') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+
         <div class="mb-5">
-            <label for="" class="block mb-2 text-base lg:text-xl font-semibold text-gray-900 dark:text-white">Judul Kursus</label>
-            <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+            <label class="block mb-2 text-base lg:text-xl font-semibold text-gray-900 dark:text-white">Judul Kursus</label>
+            <input type="text" name="judul" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
         </div>
 
         <div class="mb-5">
-            <label for="" class="block mb-2 text-base lg:text-xl font-semibold text-gray-900 dark:text-white">Deskripsi</label>
+            <label class="block mb-2 text-base lg:text-xl font-semibold text-gray-900 dark:text-white">Deskripsi</label>
             <div>
                 <div id="toolbar-deskripsi" class="mb-2">
                     <span class="ql-formats">
@@ -37,73 +39,91 @@
                     </span>
                 </div>
                 <div id="editor-deskripsi" class="bg-white h-60 border border-gray-300 rounded p-3 overflow-y-auto"></div>
+                <textarea name="deskripsi" hidden></textarea>
             </div>
         </div>
 
-        <div class="mb-5">
-            <label for="" class="block mb-2 text-base lg:text-xl font-semibold text-gray-900 dark:text-white">Nama Mentor</label>
-            <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-        </div>
-
         <div class="sm:flex gap-3 mb-5">
+            {{-- Tingkat Kesulitan --}}
             <div class="sm:w-1/2">
-                <label for="" class="block mb-2 text-base lg:text-xl font-semibold text-gray-900 dark:text-white">Tingkat Kesulitan</label>
-                <select name="" id="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                    <option value="" disabled selected>Pilih Tingkat Kesulitan</option>
-                    <option value="">Pemula</option>
-                    <option value="">Menengah</option>
-                    <option value="">Lanjutan</option>
+                <label class="block mb-2 text-base lg:text-xl font-semibold text-gray-900 dark:text-white">Tingkat Kesulitan</label>
+                <select name="tingkat_kesulitan" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option value="" disabled {{ old('tingkat_kesulitan') ? '' : 'selected' }}>Pilih Tingkat Kesulitan</option>
+                    <option value="Pemula" {{ old('tingkat_kesulitan') == 'Pemula' ? 'selected' : '' }}>Pemula</option>
+                    <option value="Menengah" {{ old('tingkat_kesulitan') == 'Menengah' ? 'selected' : '' }}>Menengah</option>
+                    <option value="Lanjutan" {{ old('tingkat_kesulitan') == 'Lanjutan' ? 'selected' : '' }}>Lanjutan</option>
                 </select>
             </div>
 
+            {{-- Kategori --}}
             <div class="sm:w-1/2 sm:mt-0 mt-5">
-                <label for="" class="block mb-2 text-base lg:text-xl font-semibold text-gray-900 dark:text-white">Kategori</label>
-                <select name="" id="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                    <option value="" disabled selected>Pilih Kategori</option>
-                    <option value="">Pengelasan</option>
-                    <option value="">Pengecatan</option>
-                    <option value="">Pemotongan</option>
+                <label class="block mb-2 text-base lg:text-xl font-semibold text-gray-900 dark:text-white">Kategori</label>
+                <select name="kategori_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option value="" disabled {{ old('kategori_id') ? '' : 'selected' }}>Pilih Kategori</option>
+                    @foreach ($kategori as $kat)
+                    <option value="{{ $kat->kategori_id }}" {{ old('kategori_id') == $kat->kategori_id ? 'selected' : '' }}>
+                        {{ $kat->nama_kategori }}
+                    </option>
+                    @endforeach
                 </select>
             </div>
         </div>
+
+
         <div class="sm:flex gap-3 mb-5">
             <div class="sm:w-1/2">
-                <label for="" class="block mb-2 text-base lg:text-xl font-semibold text-gray-900">Harga</label>
-                <input type="number" class="bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                <label class="block mb-2 text-base lg:text-xl font-semibold text-gray-900">Harga</label>
+                <input type="number" name="harga" class="bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
             </div>
 
             <div class="sm:w-1/2 sm:mt-0 mt-5">
-                <label for="" class="block mb-2 text-base lg:text-xl font-semibold text-gray-900 dark:text-white">Kapasitas</label>
-                <input type="number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <label class="block mb-2 text-base lg:text-xl font-semibold text-gray-900 dark:text-white">Kapasitas</label>
+                <input type="number" name="kapasitas" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
             </div>
         </div>
 
         <div class="sm:flex gap-3 mb-5">
             <div class="sm:w-1/2">
-                <label for="" class="block mb-2 text-base lg:text-xl font-semibold text-gray-900 dark:text-white">Tanggal Mulai</label>
-                <input type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                <label class="block mb-2 text-base lg:text-xl font-semibold text-gray-900 dark:text-white">Tanggal Mulai</label>
+                <input type="date" name="tgl_mulai" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
             </div>
             <div class="sm:w-1/2 sm:mt-0 mt-5">
-                <label for="" class="block mb-2 text-base lg:text-xl font-semibold text-gray-900 dark:text-white">Tanggal Selesai</label>
-                <input type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                <label class="block mb-2 text-base lg:text-xl font-semibold text-gray-900 dark:text-white">Tanggal Selesai</label>
+                <input type="date" name="tgl_selesai" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
             </div>
         </div>
 
         <div class="sm:flex gap-3 mb-5">
             <div class="sm:w-1/2">
-                <label for="" class="block mb-2 text-base lg:text-xl font-semibold text-gray-900 dark:text-white">Lokasi</label>
-                <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                <label class="block mb-2 text-base lg:text-xl font-semibold text-gray-900 dark:text-white">Lokasi</label>
+                <input type="text" name="lokasi" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
             </div>
             <div class="sm:w-1/2 sm:mt-0 mt-5">
-                <label for="" class="block mb-2 text-base lg:text-xl font-semibold text-gray-900 dark:text-white">Foto Kursus</label>
-                <input type="file" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" accept=".jpg,.jpeg,.png,.gif" />
+                <label class="block mb-2 text-base lg:text-xl font-semibold text-gray-900 dark:text-white">Foto Kursus</label>
+                <input type="file" name="foto_kursus" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" accept=".jpg,.jpeg,.png,.gif" />
             </div>
         </div>
 
-        <a href="/tambahjadwal" type="submit" class="px-3 py-2 mt-5 text-sm w-30 font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        {{-- Tombol submit --}}
+        <button type="submit" class="px-3 py-2 mt-5 text-sm w-30 font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             <i class="fas fa-paper-plane mr-2"></i>Simpan
-        </a>
+        </button>
     </form>
 </main>
 <script src="https://cdn.quilljs.com/1.3.6/quill.min.js"></script>
+<script>
+    const quill = new Quill('#editor-deskripsi', {
+        theme: 'snow',
+        modules: {
+            toolbar: '#toolbar-deskripsi'
+        }
+    });
+
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function() {
+        const deskripsiInput = document.querySelector('textarea[name="deskripsi"]');
+        deskripsiInput.value = quill.root.innerHTML;
+    });
+</script>
+
 @endsection
